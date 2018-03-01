6a508998972914286766121121b4061cbf861457
quyendq <quyendq.dtag@gmail.com>
2018-03-01 08:06:38 +0700

    update CoreLearner

--- src/main/java/com/pubvantage/entity/CoreLearner.java
+++ src/main/java/com/pubvantage/entity/CoreLearner.java
@@ -2,119 +2,141 @@ package com.pubvantage.entity;
 
 
 import javax.persistence.*;
+import java.util.Date;
 
+@Entity
+@Table(name = "core_learner")
 public class CoreLearner {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "id")
-  private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
 
-  @Column(name = "optimization_rule_id")
-  private long optimizationRuleId;
+    @Column(name = "optimization_rule_id")
+    private Long optimizationRuleId;
 
-  @Column(name = "identifier")
-  private String identifier;
+    @Column(name = "identifier")
+    private String identifier;
 
-  @Column(name = "segment_values")
-  private String segmentValues;
+    @Column(name = "segment_values")
+    private String segmentValues;
 
-  @Column(name = "model_path")
-  private String modelPath;
+    @Column(name = "model_path")
+    private String modelPath;
 
-  @Column(name = "metric_predictive_values")
-  private String metricsPredictiveValues;
+    @Column(name = "metric_predictive_values")
+    private String metricsPredictiveValues;
 
-  @Column(name = "created_date")
-  @Temporal(TemporalType.TIMESTAMP)
-  private java.sql.Timestamp createdDate;
+    @Column(name = "created_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
 
-  @Column(name = "updated_date")
-  @Temporal(TemporalType.TIMESTAMP)
-  private java.sql.Timestamp updatedDate;
+    @Column(name = "updated_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
 
-  @Column(name = "math_model")
-  private String mathModel;
+    @Column(name = "math_model")
+    private String mathModel;
 
+    public CoreLearner() {
+    }
 
-  public long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+    public CoreLearner(Long optimizationRuleId, String identifier, String segmentValues, String modelPath, String metricsPredictiveValues, Date createdDate, Date updatedDate, String mathModel) {
+        this.optimizationRuleId = optimizationRuleId;
+        this.identifier = identifier;
+        this.segmentValues = segmentValues;
+        this.modelPath = modelPath;
+        this.metricsPredictiveValues = metricsPredictiveValues;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.mathModel = mathModel;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public long getOptimizationRuleId() {
-    return optimizationRuleId;
-  }
+    public Long getOptimizationRuleId() {
+        return optimizationRuleId;
+    }
 
-  public void setOptimizationRuleId(long optimizationRuleId) {
-    this.optimizationRuleId = optimizationRuleId;
-  }
+    public void setOptimizationRuleId(Long optimizationRuleId) {
+        this.optimizationRuleId = optimizationRuleId;
+    }
 
+    public String getIdentifier() {
+        return identifier;
+    }
 
-  public String getIdentifier() {
-    return identifier;
-  }
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
 
-  public void setIdentifier(String identifier) {
-    this.identifier = identifier;
-  }
+    public String getSegmentValues() {
+        return segmentValues;
+    }
 
+    public void setSegmentValues(String segmentValues) {
+        this.segmentValues = segmentValues;
+    }
 
-  public String getSegmentValues() {
-    return segmentValues;
-  }
+    public String getModelPath() {
+        return modelPath;
+    }
 
-  public void setSegmentValues(String segmentValues) {
-    this.segmentValues = segmentValues;
-  }
+    public void setModelPath(String modelPath) {
+        this.modelPath = modelPath;
+    }
 
+    public String getMetricsPredictiveValues() {
+        return metricsPredictiveValues;
+    }
 
-  public String getModelPath() {
-    return modelPath;
-  }
+    public void setMetricsPredictiveValues(String metricsPredictiveValues) {
+        this.metricsPredictiveValues = metricsPredictiveValues;
+    }
 
-  public void setModelPath(String modelPath) {
-    this.modelPath = modelPath;
-  }
+    public Date getCreatedDate() {
+        return createdDate;
+    }
 
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 
-  public String getMetricsPredictiveValues() {
-    return metricsPredictiveValues;
-  }
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
 
-  public void setMetricsPredictiveValues(String metricsPredictiveValues) {
-    this.metricsPredictiveValues = metricsPredictiveValues;
-  }
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
 
+    public String getMathModel() {
+        return mathModel;
+    }
 
-  public java.sql.Timestamp getCreatedDate() {
-    return createdDate;
-  }
+    public void setMathModel(String mathModel) {
+        this.mathModel = mathModel;
+    }
 
-  public void setCreatedDate(java.sql.Timestamp createdDate) {
-    this.createdDate = createdDate;
-  }
-
-
-  public java.sql.Timestamp getUpdatedDate() {
-    return updatedDate;
-  }
-
-  public void setUpdatedDate(java.sql.Timestamp updatedDate) {
-    this.updatedDate = updatedDate;
-  }
-
-
-  public String getMathModel() {
-    return mathModel;
-  }
-
-  public void setMathModel(String mathModel) {
-    this.mathModel = mathModel;
-  }
-
+    @Override
+    public String toString() {
+        return "CoreLearner{" +
+                "id=" + id +
+                ", optimizationRuleId=" + optimizationRuleId +
+                ", identifier='" + identifier + '\'' +
+                ", segmentValues='" + segmentValues + '\'' +
+                ", modelPath='" + modelPath + '\'' +
+                ", metricsPredictiveValues='" + metricsPredictiveValues + '\'' +
+                ", createdDate=" + createdDate +
+                ", updatedDate=" + updatedDate +
+                ", mathModel='" + mathModel + '\'' +
+                '}';
+    }
 }
