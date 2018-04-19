946c603276cde668885634159e085ff6a4f5b573
quyendq <quyendq.dtag@gmail.com>
2018-04-19 11:22:46 +0700

    config log

--- src/main/java/com/pubvantage/learner/LinearRegressionLearner.java
+++ src/main/java/com/pubvantage/learner/LinearRegressionLearner.java
@@ -5,8 +5,10 @@ import com.pubvantage.constant.MyConstant;
 import com.pubvantage.utils.ConvertUtil;
 import com.pubvantage.utils.FilePathUtil;
 import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.ml.regression.LinearRegression;
 import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -27,6 +29,8 @@ public class LinearRegressionLearner implements LearnerInterface {
     @Override
     public LinearRegressionModel generateModel(SparkSession sparkSession) {
         Dataset<Row> training = linearRegressionDataProcess.getTrainingDataForLinearRegression();
+        System.out.println("Structured Data training");
+        training.show(100, false);
         LinearRegression lr = new LinearRegression()
                 .setMaxIter(MAX_ITER)
                 .setRegParam(REG_PARAM)
@@ -37,6 +41,15 @@ public class LinearRegressionLearner implements LearnerInterface {
             LinearRegressionModel lrModel = lr.fit(training);
             // Print the coefficients and intercept for linear regression.
             System.out.println("Coefficients: " + lrModel.coefficients().toString() + " Intercept: " + lrModel.intercept());
+
+            // Summarize the model over the training set and print out some metrics.
+            LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
+            System.out.println("numIterations: " + trainingSummary.totalIterations());
+            System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
+            trainingSummary.residuals().show();
+            System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
+            System.out.println("r2: " + trainingSummary.r2());
+
             String savePath = FilePathUtil.getLearnerModelPath(
                     linearRegressionDataProcess.getOptimizationRule().getId(),
                     linearRegressionDataProcess.getIdentifier(),
